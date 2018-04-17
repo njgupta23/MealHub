1c81cdcb9eb47aa1c9d62779c85ee9301b6b887f
vagrant <vagrant@vagrant.vm>
2018-04-17 21:17:02 +0000

    made 2 separate JS scripts

--- static/helper.js
+++ static/helper.js
@@ -32,7 +32,6 @@ $(".cuisine").on("click", function() {
 
 
 
-
 // Behavior of recipe-select buttons 
 
 let button = $(".recipe-select");
@@ -278,36 +277,6 @@ function makeTracker() {
 
 // $(".nutrition").on('click', makeCharts);
 
-
-// Nutrition charts for saved recipes
-
-let ctx_donut7 = $("#fatTotal").get(0).getContext("2d");
-let ctx_donut8 = $("#carbsTotal").get(0).getContext("2d");
-let ctx_donut9 = $("#proteinTotal").get(0).getContext("2d");
-
-$.get("/fat-data.json", function(data) {
-    let fatTotalChart = new Chart(ctx_donut7, {
-                            type: "doughnut",
-                            data: data,
-                            options: options
-    });
-});
-
-$.get("/carbs-data.json", function(data) {
-    let carbsTotalChart = new Chart(ctx_donut8, {
-                            type: "doughnut",
-                            data: data,
-                            options: options
-    });
-});
-
-$.get("/protein-data.json", function(data) {
-    let proteinTotalChart = new Chart(ctx_donut9, {
-                            type: "doughnut",
-                            data: data,
-                            options: options
-    });
-});
 
 
 // To initialize popovers
